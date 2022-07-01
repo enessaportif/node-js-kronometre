2576ce4b7051c4b6d5bcf14b3146682efbbb7be7
brkekinci <burak@saportif.com>
2022-07-01 05:00:10 +0300

    sayac bitti

--- public/js/counterApplication.js
+++ public/js/counterApplication.js
@@ -1,22 +1,45 @@
+let counterHour;
+let counterMinute;
+let counterSecond;
 let counter;
+let pauseChecker = 9999;
 
-function displayCounter() {         //time type'ına dönüştürmeden önce böyle deneyeyim dedim
+function displayCounter() {    //time type'ına dönüştürmeden önce böyle deneyeyim dedim
   document.getElementById("counter").innerText = counter;
-  counter = counter - 1;
-  let count = setTimeout(displayCounter, 1000);
-  if(counter < 0){
-    document.getElementById("input-container").innerHTML = 
-    `<input type="number" id="input-time" name="input-time">
-    <button id="submit-time-btn" type="submit"></button>`;
-    return;     //neden fonksiyondan çıkmıyor anlamadım
+  counter = `${counterHour} : ${counterMinute} : ${counterSecond}`;
+  if(pauseChecker === 1){
+    return;
+  }
+  counterSecond = counterSecond - 1;
+  if(counterSecond < 0){
+    counterSecond = 59;
+    counterMinute = counterMinute -1;
+    if(counterMinute < 0){
+      counterMinute = 59;
+      counterHour = counterHour - 1
+      if(counterHour < 0 && counterMinute == 59 && counterSecond == 59){ 
+        return;
+      }
+    }
   };
+  let count = setTimeout(displayCounter, 1000);
 };
 
-
 document.getElementById("submit-time-btn").addEventListener("click", () =>{
- counter = document.getElementById("input-time").value;
- document.getElementById("counter").innerText = counter;
- displayCounter();
- document.getElementById("input-container").innerHTML = "";
+  counterSecond = document.getElementById("input-second").value;
+  counterMinute = document.getElementById("input-minute").value;
+  counterHour = document.getElementById("input-hour").value;
+  counter = `${counterHour} : ${counterMinute} : ${counterSecond}`;
+  document.getElementById("counter").innerText = counter;
+  pauseChecker = 0;
+  displayCounter();
 });
 
+document.getElementById("resume-btn").addEventListener("click", () => {
+  pauseChecker = 0;
+  displayCounter();
+});
+
+document.getElementById("pause-btn").addEventListener("click", () => {
+  pauseChecker = 1;
+})
